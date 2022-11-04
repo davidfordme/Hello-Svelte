86dee0b4f8820c68221a7b10a0584facde9210b1
David Ford <d.ford@digital-velocity.co.uk>
2022-11-04 16:48:57 +0000

    Add var $: which will be updated when referenced variables are updated

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -3,8 +3,18 @@
   const increment = () => {
     count += 1
   }
+
+  $: doubled = count * 2;
 </script>
 
 <button on:click={ increment }>
   This button has been clicked {count} time{ (count !== 1)? 's' : ''}
 </button>
+
+<p>(...which doubled is {doubled})</p>
+
+<style>
+  p {
+    font-size: 0.8rem;
+  }
+</style>
